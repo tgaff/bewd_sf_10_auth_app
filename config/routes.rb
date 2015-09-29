fb433f3fe8c710c0653a4f7b35fd846f1a5d0a8d
tgaff <tgaff@alumni.nd.edu>
2015-09-28 19:01:37 -0700

    setup routes including root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'test', to: 'static_pages#test'
 
-  get 'static_pages/test'
-
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
